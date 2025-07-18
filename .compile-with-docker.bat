e0bd407f29bba837af48dde589cd72840abb159b
Robby69400 <150088684+Robby69400@users.noreply.github.com>
2025-07-18 20:20:35 +0200

    compiled fw

--- .compile-with-docker.bat
+++ .compile-with-docker.bat
@@ -2,8 +2,8 @@
 cls
 del .\compiled-firmware\*.bin
 docker build -t uvk5 .
-docker run --rm -v %CD%\compiled-firmware:/app/compiled-firmware uvk5 /bin/bash -c "cd /app && make clean && make -s ENABLE_FR_BAND=1 ENABLE_RO_BAND=0 ENABLE_PL_BAND=0 TARGET=robzyl_V5.2beta2.en.fr && cp *packed.bin compiled-firmware/"
-docker run --rm -v %CD%\compiled-firmware:/app/compiled-firmware uvk5 /bin/bash -c "cd /app && make clean && make -s ENABLE_FR_BAND=0 ENABLE_RO_BAND=0 ENABLE_PL_BAND=1 TARGET=robzyl_V5.2beta2.pl && cp *packed.bin compiled-firmware/"
-docker run --rm -v %CD%\compiled-firmware:/app/compiled-firmware uvk5 /bin/bash -c "cd /app && make clean && make -s ENABLE_FR_BAND=0 ENABLE_RO_BAND=1 ENABLE_PL_BAND=0 TARGET=robzyl_V5.2beta2.ro && cp *packed.bin compiled-firmware/"
+docker run --rm -v %CD%\compiled-firmware:/app/compiled-firmware uvk5 /bin/bash -c "cd /app && make clean && make -s ENABLE_FR_BAND=1 ENABLE_RO_BAND=0 ENABLE_PL_BAND=0 TARGET=robzyl.en.fr && cp *packed.bin compiled-firmware/"
+docker run --rm -v %CD%\compiled-firmware:/app/compiled-firmware uvk5 /bin/bash -c "cd /app && make clean && make -s ENABLE_FR_BAND=0 ENABLE_RO_BAND=0 ENABLE_PL_BAND=1 TARGET=robzyl.pl && cp *packed.bin compiled-firmware/"
+docker run --rm -v %CD%\compiled-firmware:/app/compiled-firmware uvk5 /bin/bash -c "cd /app && make clean && make -s ENABLE_FR_BAND=0 ENABLE_RO_BAND=1 ENABLE_PL_BAND=0 TARGET=robzyl.ro && cp *packed.bin compiled-firmware/"
 time /t
 pause
